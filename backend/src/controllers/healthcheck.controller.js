0a40341141c19753325a661ae42da669d890fa60
Yashmevada2807 <yashmevada07@gmail.com>
2026-06-05 01:07:54 +0530

    added healthcheck controllers

---
+++ backend/src/controllers/healthcheck.controller.js
@@ -0,0 +1,15 @@
+import { ApiResponse } from "../utils/api-response.js";
+import { asyncHandler } from "../utils/async-handler.js";
+
+const healthcheck = asyncHandler(async (req, res) => {
+    res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                { message: "server is running" }
+            )
+        )
+})
+
+export { healthcheck }
