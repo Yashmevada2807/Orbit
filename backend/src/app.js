d7578522651f8c4c873933f7a8be02f38af332ec
Yashmevada2807 <yashmevada07@gmail.com>
2026-06-05 01:07:24 +0530

    added main routes path currently auth and healthcheck

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,6 +17,12 @@ app.use(cors({
     allowedHeaders:["Authorization", "Content-Type"]
 }))
 
+import healthCheckRouter from "./routes/healthcheck.route.js"
+import authRouter from "./routes/auth.route.js"
+
+app.use("/api/v1/healthcheck", healthCheckRouter )
+
+app.use("/api/v1/auth", authRouter)
 
 app.get("/", (req, res) => {
     res.send("hello dev");
