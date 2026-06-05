6afaa5f1c228ed32e14f0a981ea85aabb788844b
Yashmevada2807 <yashmevada07@gmail.com>
2026-06-06 00:54:29 +0530

    created auth-controller

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,7 @@
+import { ApiResponse } from "../utils/api-response.js";
+import { ApiError } from "../utils/api-error.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import { User } from "../models/user.model.js"
+
+
+    
