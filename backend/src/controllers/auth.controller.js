fd7385466eec172ee9a84b24e65293efedf5bd6d
Yashmevada2807 <yashmevada07@gmail.com>
2026-06-07 00:53:35 +0530

    created register path

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -4,4 +4,23 @@ import { asyncHandler } from "../utils/async-handler.js";
 import { User } from "../models/user.model.js"
 
 
-    
+
+const registerUser = asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body
+
+    const user = await User.findOne({email})
+    if(user) throw new ApiError(400, "User already registered");
+
+    const newUser = await User.create({
+        username,
+        email,
+        password
+    })
+
+    res.status(200).json(new ApiResponse(200, newUser,"user register successfully"))
+
+
+})
+
+
+export { registerUser }
