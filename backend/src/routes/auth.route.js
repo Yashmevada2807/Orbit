adbea8290def767d391c6d86ae689241ef242cd9
Yashmevada2807 <yashmevada07@gmail.com>
2026-06-09 00:54:09 +0530

    added login, verify-email and resend-email routes

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { registerUser, verifyUser } from "../controllers/auth.controller.js";
+import { loginUser, registerUser, resendEmailVerificationLink, verifyUser } from "../controllers/auth.controller.js";
 
 const router = Router()
 
 
 router.route("/register").post(registerUser)
-router.route("/verify-email/:token").post(verifyUser)
-
+router.route("/verify-email/:token").get(verifyUser)
+router.route("/login").post(loginUser)
+router.route("/resend-email").post(resendEmailVerificationLink)
 export default router
