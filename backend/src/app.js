38f6fceec4c3be54e2266e9c7d538bc4d25bc6e7
Yashmevada2807 <yashmevada07@gmail.com>
2026-06-09 00:47:26 +0530

    added login and resend-email routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import cookieParser from "cookie-parser"
 
 const app = express();
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"))
+app.use(cookieParser())
 
 // cors Configurations
 app.use(cors({
