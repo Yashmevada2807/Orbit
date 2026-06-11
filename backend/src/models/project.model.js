10c2504aa964e78c6b637a867a5b545fe85812c8
Yashmevada2807 <yashmevada07@gmail.com>
2026-06-11 12:00:26 +0530

    created project model

---
+++ backend/src/models/project.model.js
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from "mongoose";
+import { ProjectEnum } from "../utils/project.enum.js";
+
+
+const projectSchema = new Schema({
+    workspaceid: {
+        type: Schema.Types.ObjectId,
+        ref: "WorkSpace",
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        index: true,
+    },
+    description: {
+        type: String,
+        optional: true,
+        maxlength: [1500, "cannot exceed 1500 characters"]
+    },
+    enddate: {
+        type: Date.now()
+    },
+    members: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ],
+    tasks: [{
+        type: Schema.Types.ObjectId,
+        ref: "Task"
+    }],
+    status: {
+        type: String,
+        enum: ProjectEnum,
+        default: "InProgress"
+    }
+},
+    {
+        timestamps: true
+    })
+
+export const Project = mongoose.model("Project", projectSchema)
