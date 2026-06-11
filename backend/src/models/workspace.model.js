ba7fadd6cbc8161ed081b55ed095162d86a83f65
Yashmevada2807 <yashmevada07@gmail.com>
2026-06-11 12:01:01 +0530

    created workspace model

---
+++ backend/src/models/workspace.model.js
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const workSpaceSchema = new Schema({
+    workspacename: {
+        type: String,
+        required: true,
+        unique: true,
+        index: true
+    },
+    discription: {
+        type: String,
+        optional: true,
+        maxlength: [500, 'cannot exceed 500 characters']
+    },
+    workspaceOwner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    projects: [{
+        type: Schema.Types.ObjectId,
+        ref: "Project"
+    }],
+    members: [{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }]
+},
+    {
+        timestamps: true
+    })
+
+export const WorkSpace = mongoose.model("WorkSpace", workSpaceSchema)
