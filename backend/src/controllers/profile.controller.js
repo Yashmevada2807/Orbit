7b00543a6223843bd43681c19ae95707a0dc4212
Yashmevada2807 <yashmevada07@gmail.com>
2026-06-19 01:08:42 +0530

    added getUserProfile getCurrentUserProfile updateProfile getUsers

--- backend/src/controllers/profile.controller.js
+++ backend/src/controllers/profile.controller.js
@@ -35,5 +35,78 @@ const createProfile = asyncHandler(async (req, res) => {
         )
 })
 
+const getUsers = asyncHandler(async (req, res) => {
+    const users = await User.find().select("-password -refreshtoken -isEmailVerified -emailVerificationToken -emailVerificationExpiry -email -fullname -phonenumber -githublink -linkedinlink -portfolio -createdAt -updatedAt -__v")
 
-export { createProfile }
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                users,
+            )
+        )
+})
+
+const getUserProfile = asyncHandler(async (req, res) => {
+
+    const { username } = req.params
+    if (!username) throw new ApiError(400, "username is required")
+
+    const user = await User.findOne({ username }).select("-password -refreshtoken -isEmailVerified -emailVerificationToken -emailVerificationExpiry")
+    if (!user) throw new ApiError(404, "User does not exists")
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                user
+            )
+        )
+})
+
+const getCurrentUserProfile = asyncHandler(async (req, res) => {
+
+
+    const user = await User.findById(req.user._id).select("-password -refreshtoken -isEmailVerified -emailVerificationToken -emailVerificationExpiry")
+
+    return res  
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                user
+            )
+        )
+
+
+})
+
+const updateProfile = asyncHandler(async (req, res) => {
+    const { fullname, username, phonenumber, bio } = req.body
+
+    const updatedUserProfile = await User.findById(req.user._id).select("-password -refreshtoken -isEmailVerified -emailVerificationToken -emailVerificationExpiry")
+
+    if (fullname) updatedUserProfile.fullname = fullname
+    if (username) updatedUserProfile.username = username
+    if (phonenumber) updatedUserProfile.phonenumber = phonenumber
+    if (bio) updatedUserProfile.bio = bio
+
+    await updatedUserProfile.save()
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                updatedUserProfile,
+                "User Profile Updated Successfully."
+            )
+        )
+
+
+})
+
+
+export { createProfile, getUsers, getCurrentUserProfile, updateProfile, getUserProfile }
