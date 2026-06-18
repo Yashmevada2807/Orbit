529c3f7cb52fcd63a5aaf22dc854b859e662a573
Yashmevada2807 <yashmevada07@gmail.com>
2026-06-19 01:10:01 +0530

    removed logs

--- backend/src/utils/send-verification-email.js
+++ backend/src/utils/send-verification-email.js
@@ -2,8 +2,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 import nodemailer from "nodemailer"
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
