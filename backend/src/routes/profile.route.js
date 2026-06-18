6b1893d7960a00cf816833c5bb56a6cce3d9c0c9
Yashmevada2807 <yashmevada07@gmail.com>
2026-06-19 01:09:20 +0530

    added all routes for get usersProfile and updateUserProfile

--- backend/src/routes/profile.route.js
+++ backend/src/routes/profile.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createProfile } from "../controllers/profile.controller.js";
+import { createProfile, getCurrentUserProfile, getUserProfile, updateProfile } from "../controllers/profile.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -8,6 +8,9 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
 
 router.route("/").post(verifyJWT, upload.single("avatar"), createProfile)
+router.route("/").patch(verifyJWT, updateProfile)
+router.route("/").get(verifyJWT, getCurrentUserProfile)
+router.route("/:username").get(verifyJWT, getUserProfile)
 
 
 export default router
