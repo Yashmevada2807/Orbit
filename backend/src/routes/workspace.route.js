86f5a1476afd21f0b6cd41a6bced4bb924e64e27
Yashmevada2807 <yashmevada07@gmail.com>
2026-06-20 11:54:14 +0530

    added getWorkspace getWorkspaceById updateWorkspace route

--- backend/src/routes/workspace.route.js
+++ backend/src/routes/workspace.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createWorkspace } from "../controllers/workspace.controller.js";
+import { createWorkspace, getProjects, getWorkspace, getWorkspaceById, updateWorkspace } from "../controllers/workspace.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { createProject } from "../controllers/project.controller.js";
 
@@ -8,6 +8,10 @@ const router = Router()
 
 
 router.route("/").post(verifyJWT, createWorkspace)
+router.route("/").get(verifyJWT, getWorkspace)
+router.route("/:workspaceId").get(verifyJWT, getWorkspaceById)
+router.route("/:workspaceId").put(verifyJWT, updateWorkspace)
 router.route("/:workspaceId/projects").post(verifyJWT, createProject)
+router.route("/:workspaceId/projects").get(verifyJWT, getProjects)
 
 export default router
