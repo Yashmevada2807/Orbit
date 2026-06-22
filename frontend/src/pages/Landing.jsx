e12a73b3a4817c6ca2a4bfb73b088e63c5ec75f5
Yashmevada2807 <yashmevada07@gmail.com>
2026-06-22 23:08:11 +0530

    created Dashboard, Landing, Login, Profile, Project, SignUp, Workspace pages

---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header'
+import Info from '../components/Info'
+
+const Landing = () => {
+  return (
+    <div>
+      <Header/>
+      <Info/>
+    </div>
+  )
+}
+
+export default Landing
