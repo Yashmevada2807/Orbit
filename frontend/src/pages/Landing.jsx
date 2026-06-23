1347861c72ad36ffc28cfbc044d705428a578c25
Yashmevada2807 <yashmevada07@gmail.com>
2026-06-23 23:49:45 +0530

    added some UI

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from '../components/Header'
-import Info from '../components/Info'
+import Header from '../sections/Header'
+import Info from '../sections/Info'
 
 const Landing = () => {
   return (
