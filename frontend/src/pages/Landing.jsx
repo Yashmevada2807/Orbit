50b41ec71d1da3c3d98108790f47e75fb06e75da
Yashmevada2807 <yashmevada07@gmail.com>
2026-06-25 11:59:29 +0530

    added Overview section  in landingPage

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../sections/Header'
 import Info from '../sections/Info'
+import Overview from '../sections/Overview'
 
 const Landing = () => {
   return (
     <div>
       <Header/>
       <Info/>
+      <Overview/>
     </div>
   )
 }
