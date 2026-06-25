033e03f39758146349abe023e6d0667bc729a5e5
Yashmevada2807 <yashmevada07@gmail.com>
2026-06-25 23:42:05 +0530

    added CreatAccount section

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import Header from '../sections/Header'
 import Info from '../sections/Info'
 import Overview from '../sections/Overview'
+import HowItWorks from '../sections/HowItWorks'
+import CreateAccount from '../sections/CreateAccount'
 
 const Landing = () => {
   return (
-    <div>
+    <div className='flex flex-col  justify-center'>
       <Header/>
       <Info/>
       <Overview/>
+      <HowItWorks/>
+      <div className='py-10 mt-12 px-7 flex justify-center items-center'>
+      <CreateAccount/>
+      </div>
     </div>
   )
 }
